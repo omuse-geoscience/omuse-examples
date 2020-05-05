a64240d2df02ee7c4c9f8bbc6eae0eebf0bacf59
ipelupessy <i.pelupessy@esciencecenter.nl>
2020-05-05 13:04:48 +0200

    edit era5 example

--- era5_interface/era5_plot.py
+++ era5_interface/era5_plot.py
@@ -1,18 +1,20 @@
 import time
 
 import matplotlib
-matplotlib.use("TkAgg") # slect appropiate backend
+matplotlib.use("TkAgg") # select appropiate backend
 from matplotlib import pyplot
 
 from omuse.units import units
 
-from omuse.community.era5.interface import era5cached
+from omuse.community.era5.interface import ERA5
 
-e=era5cached(variables=["2m_temperature", "total_precipitation"], nwse_boundingbox=[70, -15, 40, 15]| units.deg)
+e=ERA5(variables=["2m_temperature", "total_precipitation"], 
+       nwse_boundingbox=[70, -15, 40, 15]| units.deg)
 
+print("starting date:", e.start_datetime)
 print(e.grid) # note grid has prepended the names with _ (because long names are not valid python var names)
 
-f=pyplot.figure()
+f=pyplot.figure(figsize=(8,8))
 pyplot.ion()
 pyplot.show()
 
@@ -22,10 +24,15 @@ tend=7. | units.day
 while e.model_time < tend:
     print("tnow:",e.model_time)
     e.evolve_model(e.model_time+dt)
-    t2=e.grid._2m_temperature
+    val=e.grid._2m_temperature.value_in(units.K)
+    val=0.25*(val[:-1,:-1]+val[1:,:-1]+val[:-1,1:]+val[1:,1:])
+    x=e.grid.lon.value_in(units.deg)
+    y=e.grid.lat.value_in(units.deg)
         
     pyplot.clf()
-    pyplot.imshow(t2.value_in(units.K),vmin=260,vmax=288)
+    pyplot.pcolormesh(x,y,val,vmin=260,vmax=288)
+    pyplot.xlabel("lon (deg)")
+    pyplot.ylabel("lat (deg)")
     f.canvas.flush_events()
 
     time.sleep(1/20.)
